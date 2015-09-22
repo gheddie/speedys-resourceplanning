83ca50f40b4a66692bb0291d57a1af459c6589c2
Stefan.Schulz <Stefan.Schulz@MD1FB0AC.ww009.siemens.net>
2015-09-22 21:43:27 +0200

    third commit

--- resourceplanning-entity/src/main/java/de/trispeedys/resourceplanning/HibernateUtil.java
+++ resourceplanning-entity/src/main/java/de/trispeedys/resourceplanning/HibernateUtil.java
@@ -7,60 +7,51 @@ import org.hibernate.cfg.Configuration;
 
 import de.trispeedys.resourceplanning.entity.AbstractDbObject;
 
-public class HibernateUtil
-{
+public class HibernateUtil {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	@SuppressWarnings("deprecation")
-	private static SessionFactory buildSessionFactory()
-	{
-		try
-		{
+	private static SessionFactory buildSessionFactory() {
+		try {
 			return new Configuration().configure().buildSessionFactory();
-		} catch (Throwable ex)
-		{
+		} catch (Throwable ex) {
 			System.err.println("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
 
-	public static SessionFactory getSessionFactory()
-	{
+	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 
-	public static void shutdown()
-	{
+	public static void shutdown() {
 		getSessionFactory().close();
 	}
-	
-	public static void clearAll()
-	{
-	    clearTable("event_commitment");
-	    clearTable("helper");
-	    clearTable("position");
-	    clearTable("event_occurence");
+
+	public static void clearAll() {
+		clearTable("event_commitment");
+		clearTable("helper");
+		clearTable("position");
+		clearTable("event_occurence");
 	}
 
-    private static void clearTable(String tableName)
-    {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-        String queryString = "delete from " + tableName;
+	private static void clearTable(String tableName) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		String queryString = "delete from " + tableName;
 
-        session.createSQLQuery(queryString).executeUpdate();
-        tx.commit();
-        session.close();
-    }
-    
-    public static AbstractDbObject persistSimple(AbstractDbObject entity)
-    {
-        Transaction tx = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        tx = session.beginTransaction();
-        session.save(entity);
-        tx.commit();
-        session.close();
-        return entity;
-    }
+		session.createSQLQuery(queryString).executeUpdate();
+		tx.commit();
+		session.close();
+	}
+
+	public static AbstractDbObject persistSimple(AbstractDbObject entity) {
+		Transaction tx = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		tx = session.beginTransaction();
+		session.save(entity);
+		tx.commit();
+		session.close();
+		return entity;
+	}
 }
