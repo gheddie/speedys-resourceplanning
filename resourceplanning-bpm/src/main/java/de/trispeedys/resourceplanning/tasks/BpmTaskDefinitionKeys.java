5f28b3de4148068ed27db3a624d44be68e2bbbb6
Stefan.Schulz <Stefan.Schulz@MD1FB0AC.ww009.siemens.net>
2015-09-29 21:58:03 +0200

    minor enhancements

---
+++ resourceplanning-bpm/src/main/java/de/trispeedys/resourceplanning/tasks/BpmTaskDefinitionKeys.java
@@ -0,0 +1,9 @@
+package de.trispeedys.resourceplanning.tasks;
+
+public class BpmTaskDefinitionKeys
+{
+    public class RequestHelpHelper
+    {
+        public static final String TASK_DEFINITION_KEY_MANUAL_ASSIGNMENT = "UserTaskManualAssignment";
+    }
+}
--- resourceplanning-bpm/src/main/java/de/trispeedys/resourceplanning/util/ResourcePlanningUtil.java
+++ resourceplanning-bpm/src/main/java/de/trispeedys/resourceplanning/util/ResourcePlanningUtil.java
@@ -2,12 +2,18 @@ package de.trispeedys.resourceplanning.util;
 
 import java.text.MessageFormat;
 
+import de.trispeedys.resourceplanning.exception.DataModelException;
+
 public class ResourcePlanningUtil
 {
     private static final String BK_REQUEST_HELP_HELPERPROCESS_TEMPLATE = "bkRequestHelpHelperProcess_helper:{0}||event:{1}";
 
     public static String generateRequestHelpBusinessKey(Long helperId, Long eventId)
     {
+        if ((helperId == null) || (eventId == null))
+        {
+            throw new DataModelException("helper id AND event id must be set in order to generate a business key!!");
+        }
         return new MessageFormat(BK_REQUEST_HELP_HELPERPROCESS_TEMPLATE).format(new Object[] {helperId, eventId});
     }
 }
